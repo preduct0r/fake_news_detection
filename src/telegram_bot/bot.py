b674428ba74fdffcbd1bc285295ebce1befee0b3
kotov-d <kotov-d@speechpro.com>
2022-11-01 17:53:39 +0300

    Catch exceptions in more sofisticated way

--- src/telegram_bot/bot.py
+++ src/telegram_bot/bot.py
@@ -17,14 +17,19 @@ bot.
 
 
 import datetime
+import html
+import json
 import logging
+import traceback
 
 import configargparse
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater, JobQueue
+from telegram import ParseMode, Update
+from telegram.ext import CommandHandler, Filters, JobQueue, MessageHandler, Updater
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
@@ -43,12 +48,12 @@ args = parser.parse_args()
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text("Hi!")
 
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text("Help!")
 
 
 def echo(update, context):
@@ -58,37 +63,58 @@ def echo(update, context):
 
 def error(update, context):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-    
-    
+    logger.warning(context.error)
+
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an request\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=message, parse_mode=ParseMode.HTML
+    )
+
+
 def repeating_job(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Setting a daily notifications!')
-    
-    # job_queue.run_repeating(callback = notify_assignees, 
-    #                         interval = 5,
-    #                         first=0,
-    #                         context=update)
+    # context.bot.send_message(chat_id=update.effective_chat.id, text='Setting a daily notifications!')
+
+    context.job_queue.run_repeating(
+        callback=notify_assignees, 
+        interval=5, 
+        first=0, 
+        context=update
+    )
+
 
 def notify_assignees(bot, job):
     bot.send_message(chat_id="5629691617", text="Some text!")
-    
-    
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     updater = Updater(args.telegram_token, use_context=True)
     dp = updater.dispatcher
-    
-    dp.add_handler(CommandHandler('notify', repeating_job, pass_job_queue=True))
+
+    dp.add_handler(CommandHandler("notify", repeating_job, pass_job_queue=True))
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
-    
+
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
-
